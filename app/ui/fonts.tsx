e8a526ca8e89b5149f04f578e5f6ef8dfcbb76eb
Moritz Halke <moritz.halke@divis.io>
2024-12-09 01:47:27 +0100

    add font neuekabel BOLD

--- app/ui/fonts.tsx
+++ app/ui/fonts.tsx
@@ -2,3 +2,4 @@ import localFont from 'next/font/local';
 
 export const titleFont = localFont({ src: 'copperplategothic_bold-webfont.woff2' });
 export const textFont = localFont({ src: 'NeueKabelW01Regular.woff2' });
+export const textFontBold = localFont({ src: 'NeueKabelW01Bold.woff2' });
